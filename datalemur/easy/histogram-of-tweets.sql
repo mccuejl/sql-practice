6cba771f5bde89164b2afe593a5749bf090ab020
Jamie McCue <mccuejl@gmail.com>
2026-08-04 09:54:33 -0600

    Add Histogram of Tweets (DataLemur) solution

---
+++ datalemur/easy/histogram-of-tweets.sql
@@ -0,0 +1,16 @@
+-- Problem: Histogram of Tweets (Easy)
+-- https://datalemur.com/questions/sql-histogram-tweets
+-- First attempts were a struggle:
+--  New to applying business logic outside of a course
+--  Limited experience with subqueries/CTE 
+
+SELECT 
+tweet_count_per_user AS tweet_bucket,
+COUNT(user_id) AS users_num
+FROM (
+SELECT user_id, COUNT(tweet_id) AS tweet_count_per_user
+FROM tweets
+WHERE tweet_date BETWEEN '2022-01-01' AND '2022-12-31'
+GROUP BY user_id
+) AS total_tweets
+GROUP BY tweet_count_per_user;
